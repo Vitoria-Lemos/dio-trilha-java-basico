cb6c4da326daba6914ea6acec8280f2e9d541e7a
Vitoria Lemos <vitoria.lemos183@gmail.com>
2024-05-02 20:34:17 -0300

    Projeto finalizado

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+       
+        System.out.println("Digite o Nome do Cliente: ");
+        String nomeCliente = scan.nextLine();
+
+        System.out.print("Digite o Número: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Digite a Agência:");
+        String agencia = scan.next();
+        
+        
+        System.out.println("Digite seu Saldo: ");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+" e seu saldo "+saldo+" já está disponível para saque.");
+    
+
     }
 }
